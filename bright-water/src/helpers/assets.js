ca92c92b9dbd63c532e37a3dbe3667c0f866f09d
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-18 13:46:49 +0000

    chore: Add helpers

---
+++ bright-water/src/helpers/assets.js
@@ -0,0 +1,23 @@
+import cart from "assets/icons/cart-icon.svg";
+import logo from "assets/images/logo.png";
+import menuIcon1 from "assets/icons/menu-icon-close.svg";
+import menuIcon2 from "assets/icons/menu-icon.svg";
+
+export const cartIcon = {
+  src: cart,
+  alt: "cart icon",
+};
+
+export const logoIcon = {
+  src: logo,
+  alt: "logo icon",
+};
+
+export const menuIconOpen = {
+  src: menuIcon2,
+  alt: "mobile menu icon open",
+};
+export const menuIconClose = {
+  src: menuIcon1,
+  alt: "mobile menu icon close",
+};
