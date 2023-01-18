9b053e018fe3600b0d3f637dc27a32501054cd42
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-18 18:24:59 +0000

    chore: Add new icons and update assets file

--- bright-water/src/helpers/assets.js
+++ bright-water/src/helpers/assets.js
@@ -2,6 +2,7 @@ import cart from "assets/icons/cart-icon.svg";
 import logo from "assets/images/logo.png";
 import menuIcon1 from "assets/icons/menu-icon-close.svg";
 import menuIcon2 from "assets/icons/menu-icon.svg";
+import filter from "assets/icons/filter.svg";
 
 export const cartIcon = {
   src: cart,
@@ -17,7 +18,13 @@ export const menuIconOpen = {
   src: menuIcon2,
   alt: "mobile menu icon open",
 };
+
 export const menuIconClose = {
   src: menuIcon1,
   alt: "mobile menu icon close",
+};
+
+export const filterIcon = {
+  src: filter,
+  alt: "filter icon",
 };
