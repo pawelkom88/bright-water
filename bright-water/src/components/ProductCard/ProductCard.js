85828b088fa426a5ce03c1a8bca534590e83b2f7
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-18 13:47:59 +0000

    chore: Update imports in components

--- bright-water/src/components/ProductCard/ProductCard.js
+++ bright-water/src/components/ProductCard/ProductCard.js
@@ -1,4 +1,4 @@
-import ViewProductButton from "../ViewProductButton/ViewProductButton";
+import ViewProductButton from "components/ViewProductButton/ViewProductButton";
 import classes from "./productCard.sass";
 
 export default function ProductCard({ product }) {
