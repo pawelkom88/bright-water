dfa62522a7d593c7105a5c713a68c04e171ec5b3
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-19 17:02:21 +0000

    feat: Add functions

--- bright-water/src/helpers/helpers.js
+++ bright-water/src/helpers/helpers.js
@@ -2,3 +2,25 @@ export const navItems = [
   { name: "Home", link: "/" },
   { name: "About", link: "/about" },
 ];
+
+export function stripHTMLTag(str) {
+  return str?.replace(/(<([^>]+)>)/gi, "");
+}
+
+export function removeDuplicateObjects(array, property) {
+  const uniqueIds = [];
+
+  const unique = array?.filter(element => {
+    const isDuplicate = uniqueIds.includes(element[property]);
+
+    if (!isDuplicate) {
+      uniqueIds.push(element[property]);
+
+      return true;
+    }
+
+    return false;
+  });
+
+  return unique;
+}
