285bf3e8c926d257901518cf2df04357fe09a166
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-19 18:49:23 +0000

    feat: Add About page and styles

--- bright-water/src/pages/about/About.js
+++ bright-water/src/pages/about/About.js
@@ -1,9 +1,9 @@
-import classes from "./about.sass";
+import classes from "./About.module.scss";
 
 export default function About() {
   return (
-    <>
+    <div className={`container ${classes.styles}`}>
       <h1>About Page</h1>
-    </>
+    </div>
   );
 }
