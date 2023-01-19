fc2e730284bec34d1315c0e348a7f5882c8d36e0
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-19 21:06:11 +0000

    fix: Move state down to avoid unnecessary re-renders

--- bright-water/src/pages/home/Home.js
+++ bright-water/src/pages/home/Home.js
@@ -5,24 +5,13 @@ import Modal from "components/modal/Modal";
 import Banner from "components/banner/Banner";
 
 export default function Home() {
-  const [openModal, setOpenModal] = useState(false);
-
-  // Show modal after 5s
-  useEffect(() => {
-    const timer = setTimeout(() => setOpenModal(true), 5000);
-
-    return () => clearTimeout(timer);
-  }, [setOpenModal]);
-
   return (
     <>
       <TopBannerSection />
       <ProductsList />
-      {openModal && (
-        <Modal size="30rem">
-          <Banner />
-        </Modal>
-      )}
+      <Modal size="30rem">
+        <Banner />
+      </Modal>
     </>
   );
 }
