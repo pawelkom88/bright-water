1f0eb0e766782792ad740f0a30fc840d38a7057b
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-20 11:23:11 +0000

    Fix: Fix mobile menu

--- bright-water/src/components/NavBar/menu-mobile/MobileMenuBtn.js
+++ bright-water/src/components/NavBar/menu-mobile/MobileMenuBtn.js
@@ -1,20 +1,36 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import MenuLeft from "components/sidebar-menu/menu-left/MenuLeft";
 import Button from "components/Button/Button";
-import SidebarMenu from "components/sidebar-menu/Sidebar-menu";
-import { menuIconOpen } from "helpers/assets";
 import classes from "./MobileMenuBtn.module.scss";
+import { menuIconOpen } from "helpers/assets";
+import { navItems } from "helpers/helpers";
 
 export default function MobileMenuBtn() {
-  const [toggleIcon, setToggleIcon] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  function handleSidebar() {
+    setToggleMenu(!toggleMenu);
+  }
 
   return (
     <>
-      <Button onClick={() => setToggleIcon(!toggleIcon)} className={classes["mobile-menu-button"]}>
+      <Button onClick={() => setToggleMenu(!toggleMenu)} className={classes["mobile-menu-button"]}>
         <img src={menuIconOpen.src} alt={menuIconOpen.alt} />
       </Button>
-      <SidebarMenu onToggle={setToggleIcon} condition={toggleIcon}>
-        Menu
-      </SidebarMenu>
+      <MenuLeft onToggle={handleSidebar} condition={toggleMenu}>
+        <nav className={classes.navigation}>
+          <ul className={classes["navigation-items"]}>
+            {navItems.map((item, index) => (
+              <li className={classes["navigation-item"]} key={index}>
+                <Link onClick={() => setToggleMenu(false)} to={item.link}>
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </MenuLeft>
     </>
   );
 }
