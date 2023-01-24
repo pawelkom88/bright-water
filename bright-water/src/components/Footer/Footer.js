225043b8dee64565df312abc19beb70c8a00694c
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-24 18:44:58 +0000

    styles: Fix font size, margin,padding

--- bright-water/src/components/Footer/Footer.js
+++ bright-water/src/components/Footer/Footer.js
@@ -2,23 +2,28 @@ import { Link } from "react-router-dom";
 import Button from "components/Button/Button";
 import classes from "./Footer.module.scss";
 import { socialIcons } from "helpers/assets";
+import { useState } from "react";
 
 const date = new Date().getFullYear();
 
 export default function Footer({ navItems }) {
+  const [email, setEmail] = useState("");
+
+  function handleEmail(e) {
+    setEmail(e.target.value);
+  }
+
   return (
     <footer className={classes.footer}>
       <div className="container">
         <section className={classes["footer-top"]}>
           <div className={classes["footer-col"]}>
             <h3 className={classes["footer-col__heading"]}>Navigation</h3>
-            <ul>
-              {navItems.map((item, index) => (
-                <li key={index}>
-                  <Link to={item.link}>{item.name}</Link>
-                </li>
-              ))}
-            </ul>
+            {navItems.map((item, index) => (
+              <Link key={index} to={item.link}>
+                {item.name}
+              </Link>
+            ))}
           </div>
           <div className={classes["footer-col"]}>
             <h3 className={classes["footer-col__heading"]}>Top Products</h3>
@@ -32,11 +37,13 @@ export default function Footer({ navItems }) {
             <div className={classes["footer-col__newsletter"]}>
               <label htmlFor="newsletter">
                 <input
+                  onChange={handleEmail}
                   className={classes["footer-col__input"]}
                   type="email"
                   id="newsletter"
                   name="newsletter"
                   placeholder="Your Email Address"
+                  value={email}
                 />
               </label>
               <Button className={classes.button}>Subscribe</Button>
