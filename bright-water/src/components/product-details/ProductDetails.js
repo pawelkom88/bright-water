7698266239f830f3be5d2df04eef438d108945ae
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-24 20:06:07 +0000

    refactor: Extract code to smaller components

--- bright-water/src/components/product-details/ProductDetails.js
+++ bright-water/src/components/product-details/ProductDetails.js
@@ -1,30 +1,16 @@
+import CardDetails from "components/Card-Details/CardDetails";
 import { useState } from "react";
+import CardSlider from "components/Card-Slider/CardSlider";
 import CardNav from "components/product-details/card-nav/CardNav";
 import Notification from "components/notification/Notification";
-import QuantityStepper from "components/Quantity-stepper/QuantityStepper";
-import Button from "components/Button/Button";
-import { stripHTMLTag, removeDuplicateObjects } from "helpers/helpers";
 import classes from "./ProductDetails.module.scss";
 
-const randomClr = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-const options = Array(4).fill({ color: randomClr });
-
 export default function ProductDetails({ cartItems, product, onAdd }) {
-  const { name, assets, image, description, price } = product || {};
-
+  const { name, assets, image } = product || {};
   const [selectedImage, setSelectedImage] = useState(null);
   const [addToWishlist, setAddToWishlist] = useState(false);
   const [notifyUser, setNotifyUser] = useState(false);
   const [borderClr, setBorderClr] = useState("");
-
-  function handleBottleColor(color) {
-    setBorderClr(color);
-  }
-
-  // some images are duplicated
-  const filteredAssets = removeDuplicateObjects(assets, "id");
-
-  const modifiedDescription = stripHTMLTag(description);
 
   function AddToCartHandler(product) {
     // reset state
@@ -67,7 +53,6 @@ export default function ProductDetails({ cartItems, product, onAdd }) {
     <>
       <div className={classes.card}>
         <CardNav setAddToWishlist={setAddToWishlist} addToWishlist={addToWishlist} />
-
         <div className={classes["card-body"]}>
           <div className={classes["card-image"]}>
             <img
@@ -75,49 +60,9 @@ export default function ProductDetails({ cartItems, product, onAdd }) {
               src={selectedImage ? selectedImage : image?.url}
               alt={name}
             />
-
-            <div className={classes["card-images"]}>
-              {filteredAssets?.map(({ id, url }) => {
-                // remove white space
-                const modifiedUrl = url.replace(/ /g, ""); //some urls are broken, so could not display image correctly
-
-                const alt = url.split("|")[1];
-                return (
-                  <div key={id}>
-                    <Button onClick={() => setSelectedImage(modifiedUrl)}>
-                      <img src={modifiedUrl} alt={alt} />
-                    </Button>
-                  </div>
-                );
-              })}
-            </div>
+            <CardSlider assets={assets} onSelect={setSelectedImage} />
           </div>
-
-          <div className={classes["card-description"]}>
-            <h1 className={classes["card-description__heading"]}>{name}</h1>
-            <p className={classes["card-description__content"]}>{modifiedDescription}</p>
-            <div className={classes["color-selection"]}>
-              {options?.map(({ color }, index) => {
-                return (
-                  <div
-                    onClick={() => handleBottleColor(color)}
-                    tabIndex={0}
-                    style={{
-                      backgroundColor: randomClr,
-                    }}
-                    key={index}
-                    className={classes.circle}></div>
-                );
-              })}
-            </div>
-            <p className={classes["card-description__price"]}>{price.formatted_with_symbol}</p>
-            <div className={classes["card-description__action"]}>
-              <Button onClick={() => AddToCartHandler(product)} className={classes.button}>
-                Add to Cart
-              </Button>
-              <QuantityStepper />
-            </div>
-          </div>
+          <CardDetails cartHandler={AddToCartHandler} product={product} onSet={setBorderClr} />
         </div>
       </div>
 
