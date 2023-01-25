872312dc3eb97783cf5b9fe6837ac26b071b39c7
pawelkom88e <pawel.komorkiewicz@yahoo.co.uk>
2023-01-25 21:02:47 +0000

    feat: Add animation on image change

--- bright-water/src/components/Card-Slider/CardSlider.js
+++ bright-water/src/components/Card-Slider/CardSlider.js
@@ -6,18 +6,16 @@ export default function CardSlider({ assets, onSelect }) {
   const filteredAssets = removeDuplicateObjects(assets, "id");
 
   return (
-    <div className={classes["card-images"]}>
+    <div className={classes["slider-images"]}>
       {filteredAssets?.map(({ id, url }) => {
         // remove white space
         const modifiedUrl = url.replace(/ /g, ""); //some urls are broken, so could not display image correctly
 
         const alt = url.split("|")[1];
         return (
-          <div key={id}>
-            <Button onClick={() => onSelect(modifiedUrl)}>
-              <img src={modifiedUrl} alt={alt} />
-            </Button>
-          </div>
+          <Button key={id} onClick={() => onSelect({url: modifiedUrl, animate: 1})}>
+            <img className={classes["slider-thumbnail"]} src={modifiedUrl} alt={alt} />
+          </Button>
         );
       })}
     </div>
